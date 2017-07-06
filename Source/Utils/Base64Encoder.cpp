e444d4e8114679c553095ac6f00e31c6836a3756
Koncord <stas5978@gmail.com>
2017-07-06 11:27:49 +0800

    Simplify Base64Encoder

--- Source/Utils/Base64Encoder.cpp
+++ Source/Utils/Base64Encoder.cpp
@@ -11,85 +11,81 @@
 #include "Base64Encoder.h"
 #include <cstdlib>
 
-const char *Base64Map(void) {return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";}
 const char *base64Map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+const char *Base64Map(void)
+{
+    return base64Map;
+}
 
 // 3/17/2013 must be unsigned char or else it will use negative indices
 int Base64Encoding(const unsigned char *inputData, int dataLength, char *outputData)
 {
     // http://en.wikipedia.org/wiki/Base64
 
-    int outputOffset, charCount;
-    int write3Count;
-    outputOffset=0;
-    charCount=0;
-    int j;
+    auto fnWriteBase64 = [](char *output, char base64, int &offset)
+    {
+        static int charCount = 0;
+        output[offset++] = base64;
+        if ((++charCount % 76) == 0)
+        {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+    };
 
-    write3Count=dataLength/3;
-    for (j=0; j < write3Count; j++)
+    int outputOffset = 0;
+    int write3Count = dataLength / 3;
+    int j;
+    for (j = 0; j < write3Count; j++)
     {
         // 6 leftmost bits from first byte, shifted to bits 7,8 are 0
-        outputData[outputOffset++]=base64Map[inputData[j*3+0] >> 2];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[inputData[j * 3 + 0] >> 2], outputOffset);
 
         // Remaining 2 bits from first byte, placed in position, and 4 high bits from the second byte, masked to ignore bits 7,8
-        outputData[outputOffset++]=base64Map[((inputData[j*3+0] << 4) | (inputData[j*3+1] >> 4)) & 63];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[((inputData[j * 3 + 0] << 4) | (inputData[j * 3 + 1] >> 4)) & 63], outputOffset);
 
         // 4 low bits from the second byte and the two high bits from the third byte, masked to ignore bits 7,8
-        outputData[outputOffset++]=base64Map[((inputData[j*3+1] << 2) | (inputData[j*3+2] >> 6)) & 63]; // Third 6 bits
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[((inputData[j * 3 + 1] << 2) | (inputData[j * 3 + 2] >> 6)) & 63],  outputOffset); // Third 6 bits
 
         // Last 6 bits from the third byte, masked to ignore bits 7,8
-        outputData[outputOffset++]=base64Map[inputData[j*3+2] & 63];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[inputData[j * 3 + 2] & 63], outputOffset);
     }
 
-    if (dataLength % 3==1)
+    if (dataLength % 3 == 1)
     {
         // One input byte remaining
-        outputData[outputOffset++]=base64Map[inputData[j*3+0] >> 2];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[inputData[j * 3 + 0] >> 2], outputOffset);
 
         // Remaining 2 bits from first byte, placed in position, and 4 high bits from the second byte, masked to ignore bits 7,8
-        outputData[outputOffset++]=base64Map[((inputData[j*3+0] << 4) | (inputData[j*3+1] >> 4)) & 63];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[((inputData[j * 3 + 0] << 4) | (inputData[j * 3 + 1] >> 4)) & 63], outputOffset);
 
         // Pad with two equals
-        outputData[outputOffset++]='=';
-        outputData[outputOffset++]='=';
+        outputData[outputOffset++] = '=';
+        outputData[outputOffset++] = '=';
     }
-    else if (dataLength % 3==2)
+    else if (dataLength % 3 == 2)
     {
         // Two input bytes remaining
 
         // 6 leftmost bits from first byte, shifted to bits 7,8 are 0
-        outputData[outputOffset++]=base64Map[inputData[j*3+0] >> 2];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[inputData[j * 3 + 0] >> 2], outputOffset);
 
         // Remaining 2 bits from first byte, placed in position, and 4 high bits from the second byte, masked to ignore bits 7,8
-        outputData[outputOffset++]=base64Map[((inputData[j*3+0] << 4) | (inputData[j*3+1] >> 4)) & 63];
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[((inputData[j * 3 + 0] << 4) | (inputData[j * 3 + 1] >> 4)) & 63], outputOffset);
 
         // 4 low bits from the second byte, followed by 00
-        outputData[outputOffset++]=base64Map[(inputData[j*3+1] << 2) & 63]; // Third 6 bits
-        if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
+        fnWriteBase64(outputData, base64Map[(inputData[j * 3 + 1] << 2) & 63], outputOffset);
 
         // Pad with one equal
-        outputData[outputOffset++]='=';
-        //outputData[outputOffset++]='=';
+        outputData[outputOffset++] = '=';
     }
 
     // Append \r\n
-    outputData[outputOffset++]='\r';
-    outputData[outputOffset++]='\n';
-    outputData[outputOffset]=0;
+    outputData[outputOffset++] = '\r';
+    outputData[outputOffset++] = '\n';
+    outputData[outputOffset] = 0;
 
     return outputOffset;
-}
-
-int Base64Encoding(const unsigned char *inputData, int dataLength, char **outputData)
-{
-    *outputData = (char*) malloc(dataLength * 2 + 6);
-    return Base64Encoding(inputData, dataLength, *outputData);
 }
